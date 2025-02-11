e93c84a90c9b648c16cb39619aef3d3bfc4e4c3b
onja311 <harisoaonja06@gmail.com>
2025-02-11 16:57:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Mon Site Web</title>
-  <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-  <h1>Bienvenue sur mon site web !</h1>
-  <p>Ceci est mon site web hébergé gratuitement sur GitHub Pages.</p>
-  <script src="script.js"></script>
-</body>
-</html>
+document.addEventListener('DOMContentLoaded', function() {
+  alert('Bienvenue sur mon site web !');
+});
