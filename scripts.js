194ffd11059b1a9ee9b4cd5a4e2be60bb2168a8f
onja311 <harisoaonja06@gmail.com>
2025-02-13 08:52:08 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
-<script src="scripts.js"></script>
+function changeText() {
+    document.getElementById("demo").innerText = "Text Changed!";
+}
+
+function changeColor() {
+    document.body.style.backgroundColor = "#f0f8ff";
+}
+
+function changeColor(color) {
+    document.body.style.backgroundColor = color;
+}
+
+function showMessage(message) {
+    document.getElementById("demo").innerText = message;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    alert("Bienvenue sur notre plateforme de vente en ligne!");
+});
