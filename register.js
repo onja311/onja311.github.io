36ba0982c748fd9a73f7dd253773056ff0f08b38
onja311 <harisoaonja06@gmail.com>
2025-02-14 15:29:43 +0300

    Update register.js

--- register.js
+++ register.js
@@ -1,8 +1,10 @@
 function register() {
+    var emailOrPhone = document.getElementById("emailOrPhone").value;
     var password = document.getElementById("password").value;
     var confirmPassword = document.getElementById("confirmPassword").value;
     if (password === confirmPassword) {
-        alert("Fisoratana anarana nahomby!");
+        // Miantso ny pejy famoronana profil aorian'ny fisoratana anarana nahomby
+        window.location.href = "create_profile.html";
         return true;
     } else {
         alert("Tsy mitovy ny teny miafina. Ampidiro indray.");
