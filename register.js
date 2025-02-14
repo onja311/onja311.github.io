673a22fd6c8be66d309ccabc4ba35af8483352fe
onja311 <harisoaonja06@gmail.com>
2025-02-14 07:22:58 +0300

    Create register.js

---
+++ register.js
@@ -0,0 +1,11 @@
+function register() {
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+    if (password === confirmPassword) {
+        alert("Fisoratana anarana nahomby!");
+        return true;
+    } else {
+        alert("Tsy mitovy ny mot de passe. Ampidiro indray.");
+        return false;
+    }
+}
