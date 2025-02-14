8542f3a462efe859fa9b75003238f00d75d2cd3e
onja311 <harisoaonja06@gmail.com>
2025-02-14 08:01:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
-        document.getElementById("splash-screen").style.display = "none";
-        document.getElementById("main-content").style.display = "block";
-    }, 3000); // Mampiseho ny splash screen mandritra ny 3 segondra
+    var progressBar = document.querySelector(".progress");
+    var width = 0;
+    var interval = setInterval(function() {
+        if (width >= 100) {
+            clearInterval(interval);
+            document.getElementById("splash-screen").style.display = "none";
+            document.getElementById("main-content").style.display = "block";
+        } else {
+            width++;
+            progressBar.style.width = width + "%";
+        }
+    }, 30); // Fanitarana ny tsipika mandritra ny 3 segondra
 });
