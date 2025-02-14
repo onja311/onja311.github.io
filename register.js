27e6ae2b98293244e2b9d17413089bf316d9f821
onja311 <harisoaonja06@gmail.com>
2025-02-14 07:25:39 +0300

    Update register.js

--- register.js
+++ register.js
@@ -3,6 +3,7 @@ function register() {
     var confirmPassword = document.getElementById("confirmPassword").value;
     if (password === confirmPassword) {
         alert("Fisoratana anarana nahomby!");
+        // Misy kaody ilaina mba hitehirizana ny vaovao fisoratana anarana ao amin'ny server-nao
         return true;
     } else {
         alert("Tsy mitovy ny mot de passe. Ampidiro indray.");
