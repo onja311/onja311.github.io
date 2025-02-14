2649671171f37872ead1cd5c87e8c684b69317b9
onja311 <harisoaonja06@gmail.com>
2025-02-14 15:44:25 +0300

    Create profile_creation.js

---
+++ profile_creation.js
@@ -0,0 +1,11 @@
+function createProfile() {
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var birthDate = document.getElementById("birthDate").value;
+    var bio = document.getElementById("bio").value;
+    
+    // Ampidiro ao amin'ny tahiry (database) ny mombamomba ny profil
+    
+    alert("Profil voaforona soa aman-tsara!");
+    return false; // Manakana ny form mba hisorohana ny famerenana redirect
+}
