04c759042c3d9f32a63510f2ed99871f64c32acb
onja311 <harisoaonja06@gmail.com>
2025-02-14 07:24:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-  alert('Bienvenue sur mon site web !');
-});
+function resetPassword() {
+    var emailOrPhone = document.getElementById("emailOrPhone").value;
+    if (validateEmailOrPhone(emailOrPhone)) {
+        alert("Rohy famerenana mot de passe nalefa");
+        // Misy kaody ilaina mba handefasana ny rohy famerenana amin'ny server-nao
+    } else {
+        alert("Azafady, ampidiro ny email na nomeraon-telefaonina mety.");
+    }
+}
+
+function validateEmailOrPhone(input) {
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    var phonePattern = /^\d{10}$/;
+    return emailPattern.test(input) || phonePattern.test(input);
+}
